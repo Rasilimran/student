02d5660514adcbe2b10d54a6674fb1e1526d4618
Rasilimran <rasilimran828@gmail.com>
2024-06-10 16:08:06 +0530

    first time

---
+++ number problems/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace number_problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = 230;
+            //int count = 0;
+            //while (number != 0)
+            //{
+            //    number = number / 10;
+            //    count++;
+            //}
+            //Console.WriteLine(count);
+
+
+            //int number = 2004;
+            //int count = 0;
+            //while (number != 0)
+            //{
+            //    number = number / 10;
+            //    count++;
+            //}
+            //if (count % 2 == 0)
+            //    Console.WriteLine("even");
+
+            //else
+            //{
+            //    Console.WriteLine("odd");
+            //}
+
+            //int number = 1534;
+            //int sum = 0;
+            //while (number != 0)
+            //{
+            //    int temp = number % 10;
+            //    number = number / 10;
+            //    sum = sum + temp;
+            //}
+            //Console.WriteLine(sum);
+
+            //int number = 2123;
+            //int sum = 0;
+            //int position = 1;
+            //while (number != 0)
+            //{
+            //   int temp=number%10;
+            //    if(position%2==0)
+            //{
+            //    sum = sum + temp;
+            //}
+            //    number = number / 10;
+            //    position++;
+            //}
+            //Console.WriteLine(sum);
+
+
+            //int number = 2345;
+            //int count = 0;
+            //while (number != 0)
+            //{
+            //    int temp = number % 10;
+            //    if (temp % 2 == 0)
+            //    {
+            //        count= count+ temp;
+            //    }
+            //    number = number / 10;
+            //    temp++;
+            //}
+            //Console.WriteLine(count);
+
+
+            //int number = 2345;
+            //int sum = 0;
+            //while (number != 0)
+            //{
+            //    int temp = number % 10;
+            //    number = number / 10;
+            //    sum = sum + temp;
+            //    if (sum % 2 != 0) ;
+            //    {
+            //        }
+            //    Console.WriteLine(sum);
+            //}
+            //Console.WriteLine("even");
+
+            //int number = 256;
+            //int count = 0;
+
+            //{
+            //    number = number % 10;
+            //    count++;
+            //}
+            //Console.WriteLine(number);
+
+            //int number = 234;
+            //int rem = 0;
+            //while (number != 0)
+            //{
+            //    rem = number % 10;
+            //    number = number / 10;
+
+            //}
+            //Console.WriteLine(rem);
+
+
+            //int number = 2567;
+            //int largest = 0;
+            //while (number != 0)
+            //{
+            //    int temp = number % 10;
+            //    if (largest < temp) 
+            //    {
+            //        largest = temp;
+            //    }
+            //    number = number / 10;
+            //}
+            //Console.WriteLine(largest);
+
+            //int number = 3541;
+            //int smallest = 9;
+            //while (number != 0)
+            //{
+            //    int temp = number % 10;
+            //    if (smallest > temp)
+            //    {
+            //        smallest = temp;
+            //    }
+            //    number = number / 10;
+            //}
+            //Console.WriteLine(smallest);
+
+
+            //int number = 3421;
+            //int position = 0;
+            //int largest_dgt = 0;
+            //int i = 0;
+            //while (number != 0)
+            //{
+            //    int temp = number % 10;
+            //    if (largest_dgt < temp)
+            //    {
+            //        largest_dgt = temp;
+            //        position = i;
+
+            //    }
+
+            //    number = number / 10;
+            //    i++;
+            //}
+
+            //Console.WriteLine(Math.Pow(10, position) + "s");
+
+            //int number = 6712;
+            //int position = 0;
+            //int largest_dgt = 9;
+            //int i = 0;
+            //while (number != 0)
+            //{
+            //    int temp = number % 10;
+            //    if (largest_dgt >temp)
+            //    {
+            //        largest_dgt = temp;
+            //        position = i;
+
+            //    }
+
+            //    number = number / 10;
+            //    i++;
+            //}
+
+            //Console.WriteLine(Math.Pow(10, position) + "s");
+
+
+ //13           int number = 1234;
+ //           int reversnumber = 0;
+ //           while (number != 0) 
+ //           {
+ //               int temp = number % 10;
+ //               reversnumber =( reversnumber * 10) + temp;
+ //               number = number / 10;
+ //           }
+ //           Console.WriteLine(reversnumber);
+
+
+
+            //int number = 8654;
+            //int sum = 0;
+            //bool istrue = true;
+            //while (istrue)
+            //{
+            //    while (number != 0)
+            //    {
+            //        int temp = number % 10;
+            //        number = number / 10;
+            //        sum = sum + temp;
+            //    }
+            //    number = sum;
+            //    sum = 0;
+            //    if (number < 10)
+            //    {
+            //        istrue = false;
+            //    }
+            //    Console.WriteLine(number);
+
+
+            //int number=486;
+            //int copynumber=number;
+
+            //bool isdivide=true;
+            //while(number!=0)
+            //{
+            //    int temp
+            //}
+
+
+
+
+
+                    
+            }
+
+
+
+
+        }
+    }
+}
